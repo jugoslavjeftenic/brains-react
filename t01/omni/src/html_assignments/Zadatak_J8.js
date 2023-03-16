a8a1fea64a945937eb68bb256f5260542fce3851
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-03-16 23:20:05 +0100

    Zavrsen HTML zadatak 8.

--- t01/omni/src/html_assignments/Zadatak_J8.js
+++ t01/omni/src/html_assignments/Zadatak_J8.js
@@ -10,7 +10,23 @@ Oh, bring back my Bonnie to me.
 
 const Zadatak_J8 = () => {
   // to do
-  return <div>Zadatak 8</div>;
+  return <div>
+    <div>Zadatak 8</div>
+    <div>
+      <h1>Poem</h1>
+      <hr width="90%" />
+      <p>
+        My Bonnie lies over the ocean<br />
+        My Bonnie lies over the sea<br />
+        My Bonnie lies over the ocean<br />
+        Oh, bring back my Bonnie to me.
+      </p>
+
+
+
+
+    </div>
+  </div>;
 };
 
 export default Zadatak_J8;
