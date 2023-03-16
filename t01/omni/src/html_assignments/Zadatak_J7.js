dfe4c045e0dd00a213496d5b3f4844326b61c64b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-03-16 23:12:08 +0100

    Uradjen HTML zadatak 8.

--- t01/omni/src/html_assignments/Zadatak_J7.js
+++ t01/omni/src/html_assignments/Zadatak_J7.js
@@ -3,6 +3,7 @@
     Slika je dostupna na sledećem linku: https://www.logo.wine/logo/Apache_Tomcat
 */
 import slika from "./../assets/images/zad7.png";
+import slika2 from "./../assets/images/Apache_Tomcat-Logo.wine.png";
 
 const Zadatak_J7 = () => {
   return (
@@ -11,10 +12,39 @@ const Zadatak_J7 = () => {
       <div className="zad_container">
         <div>
           <p> Prikazati tekst kao što je na slici: </p>
-          <img src={slika} />
+          <img src={slika} alt="" />
         </div>
-        <div>{/* to do: ovde strukturirati tekst */}</div>
-        <p> TO DO:</p>
+        <div>
+          <br />
+          <h1>WEB čitači</h1>
+          <p>
+            <em>Web čitač</em> je aplikacija koja je namenjena prikazu sadržaja web stranica.
+          </p>
+          <p>
+            Stranice se mogu dobaviti kako iz fajl-sistema, tako i putem TCP/IP mreže.
+            Protokol kojim komuniciraju wb čitač i web server je HTTP protokol.
+          </p>
+          <p>
+            Danas su najpoznatiji sledeću web čitači:
+            <ol>
+              <li>
+                <a href="https://www.microsoft.com/en-us/edge" target="_blank" rel="noreferrer">Microsoft Edge</a> (https://www.microsoft.com/en-us/edge)
+              </li>
+              <li>
+                <a href="https://www.google.com/chrome" target="_blank" rel="noreferrer">Google Chrome</a> (https://www.google.com/chrome)
+              </li>
+              <li>
+                <a href="https://www.mozilla.org" target="_blank" rel="noreferrer">Mozilla</a> (https://www.mozilla.org)
+              </li>
+            </ol>
+          </p>
+          <img src={slika2} width={200} alt="" />
+
+
+
+
+          {/* to do: ovde strukturirati tekst */}
+        </div>
       </div>
     </div>
   );
