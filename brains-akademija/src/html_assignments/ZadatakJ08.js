35c5523f9666cd8c2ad3ae484642b7f96f5027ab
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-03-30 12:26:05 +0200

    Zavrsen osmi css zadatak.

--- brains-akademija/src/html_assignments/ZadatakJ08.js
+++ brains-akademija/src/html_assignments/ZadatakJ08.js
@@ -7,20 +7,26 @@ Oh, bring back my Bonnie to me.
 */
 
 // Iznad stihova napisati naslov pesme: "Poem" i odvojiti horizontalnom linijom od stihova
+import './Zadatak08_css.css';
+import Header from "./../Header";
+import Footer from "./../Footer";
 
 const Zadatak_J08 = () => {
 	// to do
 	return (
-		<div>
-			<h2> Zadatak 8</h2>
-			<h1>Poem</h1>
-			<hr width="99%" />
-			<p>
-				My Bonnie lies over the ocean<br />
-				My Bonnie lies over the sea<br />
-				My Bonnie lies over the ocean<br />
-				Oh, bring back my Bonnie to me.
-			</p>
+		<div className="wrapper html_zad08">
+			<Header />
+			<div className="sadrzaj">
+				<h1>Poem</h1>
+				<hr />
+				<p>
+					My Bonnie lies over the ocean<br />
+					My Bonnie lies over the sea<br />
+					My Bonnie lies over the ocean<br />
+					Oh, bring back my Bonnie to me.
+				</p>
+			</div>
+			<Footer />
 		</div>
 	);
 };
