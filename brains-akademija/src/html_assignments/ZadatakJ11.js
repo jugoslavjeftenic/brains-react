2caf6b98268d0dbe66a3cc7cbeffb42df26bffe3
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-03-31 13:11:59 +0200

    Zavrsen zadatak 11.

--- brains-akademija/src/html_assignments/ZadatakJ11.js
+++ brains-akademija/src/html_assignments/ZadatakJ11.js
@@ -1,85 +1,119 @@
+import './Zadatak11_css.css';
+import Header from "./../Header";
+import Footer from "./../Footer";
 import slika from './../assets/images/zadatak11.png';
 
 const today = new Date();
 
 const Zadatak_J11 = () => {
     return (
-        <div>
-            <h2> Zadatak 11</h2>
-            <div className="zad_container">
-                <div width="50%">
-                    <p> Prikazati tekst kao što je na slici: </p>
-                    <img src={slika} width="100%" alt="" />
+        <div className="wrapper html_zad11">
+            <Header />
+            <div className="zadatak margina_vrh">
+                <p> Prikazati tekst kao što je na slici: </p>
+                <img src={slika} alt="" />
+            </div>
+
+            <div className="resenje margina_vrh" id="home">
+                <div className="zaglavlje">
+                    <div>Inside of header - let's add a navbar with some essential links</div>
+                    <a href="#home"><div>Home</div></a>
+                    <a href="#about"><div>About</div></a>
+                    <a href="#contact"><div>Contact</div></a>
                 </div>
 
-                <div>
-                    <header>
-                        <p>Inside of header - let's add a navbar with some essential links</p>
-                        <p>
-                            <a href="#articles">Home </a>
-                            <a href="#about">About </a>
-                            <a href="#contact">Contact </a>
-                        </p>
-                    </header>
-                    <hr />
+                <section className="articles margina_vrh">
+                    <p><strong>Here is the main section of our page - it consist of a new articles</strong></p>
+                    <article>
+                        <h1>Article 1 - 1995 was a pretty crazy year for programming languages!</h1>
+                        <p>Don't belive me? Just check this table out:</p>
+                        <table>
+                            <tr><th>Language</th><th>Designed By</th><th>First Appeared</th></tr>
+                            <tr><td>C++</td><td>Bjarne Stroustrup</td><td>1083</td></tr>
+                            <tr><td>Python</td><td>Guido van Rossum</td><td>1991</td></tr>
+                            <tr><td>Ruby</td><td>Yukihiro Matsumoto (*Matz)</td><td>1995</td></tr>
+                            <tr><td>JavaScript</td><td>Brendan Eich</td><td>1995</td></tr>
+                            <tr><td>Java</td><td>James Gosling</td><td>1995</td></tr>
+                            <tr><td>PHP</td><td>Rasmus Lerdorf</td><td>1995</td></tr>
+                        </table>
+                    </article>
+                    <article>
+                        <h1>Article 2 - Comming soon ...</h1>
+                    </article>
+                </section>
 
-                    <main>
-                        <section id="articles">
-                            <h1>Here is the main section of our page - it consist of a new articles</h1>
-                            <article>
-                                <h2>Article 1 - 1995 was a pretty crazy year for programming languages!</h2>
-                                <p>Don't belive me? Just check this table out:</p>
-                                <table>
-                                    <tr><th>Language</th><th>Designed By</th><th>First Appeared</th></tr>
-                                    <tr><td>C++</td><td>Bjarne Stroustrup</td><td>1083</td></tr>
-                                    <tr><td>Python</td><td>Guido van Rossum</td><td>1991</td></tr>
-                                    <tr><td>Ruby</td><td>Yukihiro Matsumoto (*Matz)</td><td>1995</td></tr>
-                                    <tr><td>JavaScript</td><td>Brendan Eich</td><td>1995</td></tr>
-                                    <tr><td>Java</td><td>James Gosling</td><td>1995</td></tr>
-                                    <tr><td>PHP</td><td>Rasmus Lerdorf</td><td>1995</td></tr>
-                                </table>
-                            </article>
-                            <article>
-                                <h2>Article 2 - Comming soon ...</h2>
-                            </article>
-                        </section>
+                <section id="about"></section>
 
-                        <section id="about"></section>
+                <section className="contact margina_vrh" id="contact">
+                    <h1>Contact us!</h1>
+                    <form action="https://cscie12.dce.harvard.edu/echo" method="POST">
+                        <h2>General information</h2>
+                        <div>
+                            <label className="blok_inline desno">First Name</label>
+                            <input className="blok_inline" type="text" name="first_name" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno">Last Name</label>
+                            <input className="blok_inline" type="text" name="last_name" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno">Email</label>
+                            <input className="blok_inline" type="email" name="email" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno">Date of Birth</label>
+                            <input className="blok_inline" type="date" name="date" />
+                        </div>
 
-                        <section id="contact">
-                            <h2>Contact us</h2>
-                            <form action="https://cscie12.dce.harvard.edu/echo" method="POST">
-                                <h3>General information</h3>
-                                <div><label>First Name</label><input type="text" name="first_name" /></div>
-                                <div><label>Last Name</label><input type="text" name="last_name" /></div>
-                                <div><label>Email</label><input type="email" name="email" /></div>
-                                <div><label>Date of Birth</label><input type="date" name="date" /></div>
+                        <h2>Essential Question</h2>
+                        <p>What kind of work do you need?</p>
+                        <div>
+                            <label className="blok_inline desno" for="programming">Programming</label>
+                            <input type="checkbox" name="programming" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno" for="design">Design</label>
+                            <input type="checkbox" name="design" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno" for="singing">Singing</label>
+                            <input type="checkbox" name="singing" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno" for="comedy">Comedy</label>
+                            <input type="checkbox" name="comedy" />
+                        </div>
+                        <p>How interested are you in our design skills?</p>
+                        <div>
+                            <label className="blok_inline desno" for="design_skill">Very low</label>
+                            <input type="radio" name="design_skill" value="very_low" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno" for="design_skill">Low</label>
+                            <input type="radio" name="design_skill" value="low" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno" for="design_skill">Medium</label>
+                            <input type="radio" name="design_skill" value="medium" />
+                        </div>
+                        <div>
+                            <label className="blok_inline desno" for="design_skill">High</label>
+                            <input type="radio" name="design_skill" value="high" />
+                        </div>
 
-                                <h3>Essential Question</h3>
-                                <p>What kind of work do you need?</p>
-                                <div><label for="programming">Programming</label><input type="checkbox" name="programming" /></div>
-                                <div><label for="design">Design</label><input type="checkbox" name="design" /></div>
-                                <div><label for="singing">Singing</label><input type="checkbox" name="singing" /></div>
-                                <div><label for="comedy">Comedy</label><input type="checkbox" name="comedy" /></div>
-                                <p>How interested are you in our design skills?</p>
-                                <div><label for="design_skill">Very low</label><input type="radio" name="design_skill" value="very_low" /></div>
-                                <div><label for="design_skill">Low</label><input type="radio" name="design_skill" value="low" /></div>
-                                <div><label for="design_skill">Medium</label><input type="radio" name="design_skill" value="medium" /></div>
-                                <div><label for="design_skill">High</label><input type="radio" name="design_skill" value="high" /></div>
+                        <div className="pucad">
+                            <input type="submit" value="Submit me!" />
+                            <input type="reset" />
+                        </div>
+                    </form>
+                </section>
 
-                                <input type="submit" value="Submit me!" />
-                                <input type="reset" />
-                            </form>
-                        </section>
-                    </main>
-                    <hr />
-
-                    <footer>
-                        <p>Here is our footer - let's make shure we copyright © this.</p>
-                        <p>Copyright &copy; {today.getFullYear()}</p>
-                    </footer>
-                </div>
-            </div >
+                <footer>
+                    <p>Here is our footer - let's make shure we copyright © this.</p>
+                    <p>Copyright &copy; {today.getFullYear()}</p>
+                </footer>
+            </div>
+            <Footer />
         </div >
     );
 };
