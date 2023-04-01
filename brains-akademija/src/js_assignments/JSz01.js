f4d9c36f11979b1d9c74a0dc9ccce88ee64c5b10
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-04-01 13:21:56 +0200

    Uradjeni JS zadaci 1 i 2.

--- brains-akademija/src/js_assignments/JSz01.js
+++ brains-akademija/src/js_assignments/JSz01.js
@@ -5,35 +5,55 @@
 import Header from "./../Header";
 import Footer from "./../Footer";
 
-const sort = (niz) => {
-	niz.sort(function (a, b) {
-		return a - b;
-	});
+const zadatak = "Napiši funkciju koja prima ulazni niz proizvoljne dužine i sortira ga.";
+let niz = [8, 1, 3, 6, 3, 6, 8, 9, 10, 0];
+const ulaz = `nesortiran niz: [${niz}]`;
+
+const kod = `
+const sortiraj = (niz) => {
+	for (let i = 0; i < niz.length; i++) {
+		for (let j = i + 1; j < niz.length; j++) {
+			if (niz[j] < niz[i]) {
+				let tmp_broj = niz[i];
+				niz[i] = niz[j];
+				niz[j] = tmp_broj;
+			}
+		}
+	}
+	return niz;
+}
+`;
+
+const izlaz = `uzlazno sortiran niz: `;
+
+const sortiraj = (niz) => {
+	for (let i = 0; i < niz.length; i++) {
+		for (let j = i + 1; j < niz.length; j++) {
+			if (niz[j] < niz[i]) {
+				let tmp_broj = niz[i];
+				niz[i] = niz[j];
+				niz[j] = tmp_broj;
+			}
+		}
+	}
 	return niz;
 }
 
-const input = `let niz = [8, 1, 3, 6, 3, 6, 8, 9, 10, 0];`;
-
-const code = `const sort = (niz) => {
-	niz.sort(function (a, b) {
-		return a - b;
-	});
-	return niz;
-}`;
-
 const JSz01 = () => {
-	let niz = [8, 1, 3, 6, 3, 6, 8, 9, 10, 0];
-	sort(niz);
+	sortiraj(niz);
+
 	return (
 		<div className="wrapper js_zadatak">
 			<Header />
 			<div className="margin_top_5rem">
+				<div className="tekst">zadatak:</div>
+				<div className="code"><code>{zadatak}</code></div>
 				<div className="tekst">ulaz:</div>
-				<div className="code"><pre><code>{input}</code></pre></div>
-				<div className="tekst">kod:</div>
-				<div className="code"><pre><code>{code}</code></pre></div>
+				<div className="code"><pre><code>{ulaz}</code></pre></div>
+				<div className="tekst">metoda:</div>
+				<div className="code"><pre><code>{kod}</code></pre></div>
 				<div className="tekst">izlaz:</div>
-				<div className="code"><code> Sortiran niz: {JSON.stringify(niz)} </code></div>
+				<div className="code"><code>{izlaz}{JSON.stringify(niz)}</code></div>
 			</div>
 			<Footer />
 		</div>
--- brains-akademija/src/js_assignments/JSz02.js
+++ brains-akademija/src/js_assignments/JSz02.js
@@ -1,10 +1,58 @@
 /* 
     Zadatak 2
-    Prošriti funkciju iz zadatka 1 tako da ne sortira samo brojeve u uzlaznom redosledu nego može da sortira bilo šta kroz komparatorsku funkciju koja vraća, za neko a i b, -1 kada a < b, 0 kada a == b, i 1 kada a > b. Takva funkcija za sortiranje dobija niz i funkciju. Testirati ovu funkciju tako što joj se da niz i funkcija koja sortira u silazećem redosledu. Inače, svaki niz ima funkciju .sort koja obavlja tačno ovaj posao koji ovde kodiramo, i prima kao parametar komparatorsku funkciju koja radi tačno kao ova koju opisujemo ovde. 
+    Prošriti funkciju iz zadatka 1 tako da ne sortira samo brojeve u uzlaznom redosledu
+    nego može da sortira bilo šta kroz komparatorsku funkciju koja vraća, za neko a i b,
+    -1 kada a < b, 0 kada a == b, i 1 kada a > b.
+    Takva funkcija za sortiranje dobija niz i funkciju.
+    Testirati ovu funkciju tako što joj se da niz i funkcija koja sortira u silazećem redosledu.
+    Inače, svaki niz ima funkciju .sort koja obavlja tačno ovaj posao koji ovde kodiramo,
+    i prima kao parametar komparatorsku funkciju koja radi tačno kao ova koju opisujemo ovde. 
 */
+import Header from "./../Header";
+import Footer from "./../Footer";
 
-const JSV2 = () => {
-  return <div></div>;
+const zadatak = "Prošriti funkciju iz zadatka 1 tako da ne sortira samo brojeve u uzlaznom redosledu nego " +
+    "može da sortira bilo šta kroz komparatorsku funkciju koja vraća, za neko a i b, - 1 kada a <b, " +
+    "0 kada a == b, i 1 kada a > b. Takva funkcija za sortiranje dobija niz i funkciju. " +
+    "Testirati ovu funkciju tako što joj se da niz i funkcija koja sortira u silazećem redosledu. " +
+    "Inače, svaki niz ima funkciju .sort koja obavlja tačno ovaj posao koji ovde kodiramo, " +
+    "i prima kao parametar komparatorsku funkciju koja radi tačno kao ova koju opisujemo ovde. ";
+let niz = [8, 1, 3, 6, 3, 6, 8, 9, 10, 0];
+const ulaz = `nesortiran niz: [${niz}]`;
+
+const kod = `
+const sortiraj = (niz,) => {
+    niz.sort((a, b) => b - a);
+    return niz;
+}
+`;
+
+const izlaz = `silazno sortiran niz: `;
+
+const sortiraj = (niz,) => {
+    niz.sort((a, b) => b - a);
+    return niz;
+}
+
+const JSz02 = () => {
+    sortiraj(niz);
+
+    return (
+        <div className="wrapper js_zadatak">
+            <Header />
+            <div className="margin_top_5rem">
+                <div className="tekst">zadatak:</div>
+                <div className="code"><code>{zadatak}</code></div>
+                <div className="tekst">ulaz:</div>
+                <div className="code"><pre><code>{ulaz}</code></pre></div>
+                <div className="tekst">metoda:</div>
+                <div className="code"><pre><code>{kod}</code></pre></div>
+                <div className="tekst">izlaz:</div>
+                <div className="code"><code>{izlaz}{JSON.stringify(niz)}</code></div>
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
-export default JSV2;
+export default JSz02;
