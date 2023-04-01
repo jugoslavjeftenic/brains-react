fc846ca04399c66f39af1f5e387a005c6935d7c1
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-04-01 10:16:12 +0200

    Uradjen JS zadatak 1 + ispravka globalnog css-a.

--- brains-akademija/src/js_assignments/JSassignment.js
+++ brains-akademija/src/js_assignments/JSassignment.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import './JSassignment.css';
 import Header from "./../Header";
 import Footer from "./../Footer";
 
--- brains-akademija/src/js_assignments/JSz01.js
+++ brains-akademija/src/js_assignments/JSz01.js
@@ -1,10 +1,43 @@
 /* 
-    Zadatak 1
-    Napiši funkciju koja prima ulazni niz proizvoljne dužine i sortira ga. 
+	Zadatak 1
+	Napiši funkciju koja prima ulazni niz proizvoljne dužine i sortira ga. 
 */
+import Header from "./../Header";
+import Footer from "./../Footer";
 
-const JSV1 = () => {
-  return <div></div>;
+const sort = (niz) => {
+	niz.sort(function (a, b) {
+		return a - b;
+	});
+	return niz;
+}
+
+const input = `let niz = [8, 1, 3, 6, 3, 6, 8, 9, 10, 0];`;
+
+const code = `const sort = (niz) => {
+	niz.sort(function (a, b) {
+		return a - b;
+	});
+	return niz;
+}`;
+
+const JSz01 = () => {
+	let niz = [8, 1, 3, 6, 3, 6, 8, 9, 10, 0];
+	sort(niz);
+	return (
+		<div className="wrapper js_zadatak">
+			<Header />
+			<div className="margin_top_5rem">
+				<div className="tekst">ulaz:</div>
+				<div className="code"><pre><code>{input}</code></pre></div>
+				<div className="tekst">kod:</div>
+				<div className="code"><pre><code>{code}</code></pre></div>
+				<div className="tekst">izlaz:</div>
+				<div className="code"><code> Sortiran niz: {JSON.stringify(niz)} </code></div>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
-export default JSV1;
+export default JSz01;
