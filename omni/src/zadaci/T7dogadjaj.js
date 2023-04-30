dcce27057290e659fd8d2597fd285e71b214db4e
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-04-30 15:28:19 +0200

    Izrada zadataka.

--- omni/src/zadaci/T7dogadjaj.js
+++ omni/src/zadaci/T7dogadjaj.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const T7dogadjaj = () => {
-    let vrednost;
+    const [vrednost, setVrednost] = useState();
 
     return (
         <div>
             <div>Zdravo, ja sam komponenta koja ispisuje sadrzaj input polja u konzolu</div>
-            <input onBlur={e => vrednost = e.target.value} type='text' />
+            <input onBlur={e => setVrednost(e.target.value)} type='text' />
             <button onClick={() => console.log({ vrednost })}>Konzola</button>
+            <div>Ovde ce se ispisati vrednost preko useState funkcije: {vrednost}</div>
         </div>
     );
 };
