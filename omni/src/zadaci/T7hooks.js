e75b4a850874c802a995dd85b9cbd4a7964d2034
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-05-01 01:57:42 +0200

    Izrada zadataka.

--- omni/src/zadaci/T7hooks.js
+++ omni/src/zadaci/T7hooks.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const T7hook1 = () => {
     const [brojac, setBrojac] = useState(0);
@@ -7,9 +7,40 @@ export const T7hook1 = () => {
         <div>
             <div>Zdravo, ja sam komponenta koja ispisuje koliko puta si kliknula/o na dugme.</div>
             <button
-                onClick={e => setBrojac(1 + brojac)}
+                onClick={() => setBrojac(1 + brojac)}
             >Kliknula/o si me {brojac} puta</button>
-            <button onClick={e => setBrojac(0)}>Resetuj brojac</button>
+            <button onClick={() => setBrojac(0)}>Resetuj brojac</button>
+        </div>
+    );
+};
+
+export const T7hook2 = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [greske, setGreske] = useState("");
+
+    useEffect(() => {
+        const validatePasswordRegex = /(?=.*[A-Z])/;
+        setGreske("");
+        if (username.length < 5) {
+            setGreske("Korisnicko ime mora da ima najmanje 5 karaktera! ");
+        }
+        if (password.length < 9) {
+            setGreske(prevGreske => prevGreske + "Lozinka mora da ima najmanje 9 karaktera! ");
+        }
+        if (!validatePasswordRegex.test(password)) {
+            setGreske(prevGreske => prevGreske + "Lozinka mora da ima najmanje jedno veliko slovo! ");
+        }
+    }, [username, password]);
+
+    return (
+        <div>
+            <div>Zdravo, ja sam komponenta koja validira korisnicko ime i lozinku.</div>
+            <form>
+                <input onChange={e => setUsername(e.target.value)} type='tekst' />
+                <input onChange={e => setPassword(e.target.value)} type='password' />
+                <div>{greske}</div>
+            </form>
         </div>
     );
 };
