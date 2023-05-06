0d7ecbcaeac5f7bd9d4cc8fc5ff1ec81f7f885ba
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-05-06 13:52:22 +0200

    book-sample

--- book-sample/src/App.js
+++ book-sample/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<ul>
+					<li><NavLink to='books'>Books</NavLink></li>
+					<li><NavLink to='authors'>Authosr</NavLink></li>
+					<li><NavLink to='genres'>Genres</NavLink></li>
+				</ul>
+			</div>
+			<Outlet></Outlet>
+		</div>
+	);
 }
 
 export default App;
