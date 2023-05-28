841fe9144b8d4e565365a7463c57b1dc24890200
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-05-28 21:49:55 +0200

    Uradjen fetch predmeta. Treba zavrsiti errorHandl.

--- povratak-u-skolu/src/index.js
+++ povratak-u-skolu/src/index.js
@@ -13,7 +13,7 @@ import Administratori from './korisnici/Administratori';
 import Nastavnici from './korisnici/Nastavnici';
 
 import Predmeti from './predmeti/Predmeti';
-import NoviPredmet from './predmeti/NoviPredmet';
+// import NoviPredmet from './predmeti/NoviPredmet';
 import Predaju from './predmeti/Predaju';
 
 const router = createBrowserRouter([
@@ -69,21 +69,21 @@ const router = createBrowserRouter([
 			{
 				path: '/predmeti',
 				element: <Predmeti />,
-				errorElement: <PageError />,
-				loader: async () => {
-					const fetchGenres = await fetch(`http://localhost:8080/api/v1/predmeti`);
-					if (fetchGenres.status === 404) {
-						throw new Response("Resurs nije nađen", { status: 404 });
-					}
-					const retVal = fetchGenres;
-					return retVal;
-				},
-				children: [
-					{
-						path: 'novi-predmet',
-						element: <NoviPredmet />
-					}
-				]
+				// errorElement: <PageError />,
+				// loader: async () => {
+				// 	const fetchGenres = await fetch(`http://localhost:8080/api/v1/predmeti`);
+				// 	if (fetchGenres.status === 404) {
+				// 		throw new Response("Resurs nije nađen", { status: 404 });
+				// 	}
+				// 	const retVal = fetchGenres;
+				// 	return retVal;
+				// },
+				// children: [
+				// 	{
+				// 		path: 'novi-predmet',
+				// 		element: <NoviPredmet />
+				// 	}
+				// ]
 			},
 			// {
 			// 	path: '/predmeti/novi-predmet',
--- povratak-u-skolu/src/predmeti/Predmeti.js
+++ povratak-u-skolu/src/predmeti/Predmeti.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import { Outlet, useLoaderData, useNavigate } from 'react-router-dom';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import React, { useRef, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Box, Button, Container, FormControl, MenuItem, Select, TextField } from '@mui/material';
 
 const Predmeti = () => {
-    const predmeti = useLoaderData();
-    // console.log(predmeti);
     const navigate = useNavigate();
+    const [selectWhatToFetch, setSelectWhatToFetch] = useState(['0', '', 'none']);
+    const fetchParam = useRef('');
+    const [fetchedData, setFetchedData] = useState([]);
 
-    const prikaziPredmete = () => {
-        return null;
+    const handleFetchSelect = (e) => {
+        const choice = e.target.value;
+        switch (choice) {
+            case '1':
+                setSelectWhatToFetch(['1', 'upišite ID predmeta...', 'inline-flex']);
+                break;
+            case '2':
+                setSelectWhatToFetch(['2', 'upišite početak naziva...', 'inline-flex']);
+                break;
+            default:
+                setSelectWhatToFetch(['0', '', 'none']);
+        }
+    };
+
+    const handleFetchParam = (e) => {
+        fetchParam.current = e.target.value;
+    };
+
+    const handleFetch = () => {
+        let cancel = false;
+        const goFetch = async () => {
+            try {
+                let fetchResult;
+                switch (selectWhatToFetch[0]) {
+                    case '1':
+                        fetchResult = await fetch(`http://localhost:8080/api/v1/predmeti/${parseInt(fetchParam.current)}`);
+                        break;
+                    case '2':
+                        fetchResult = await fetch(`http://localhost:8080/api/v1/predmeti/by-naziv/${fetchParam.current}`);
+                        break;
+                    default:
+                        fetchResult = await fetch("http://localhost:8080/api/v1/predmeti");
+                }
+                let f = await fetchResult.json();
+                if (!Array.isArray(f)) {
+                    let notArrayFetched = f;
+                    f = [];
+                    f.push(notArrayFetched);
+                }
+                if (!cancel) {
+                    setFetchedData(f);
+                }
+            }
+            catch (error) {
+                console.error("Doslo je do greske: " + error.message);
+            }
+            finally {
+                //
+            }
+        };
+        goFetch();
+        return () => {
+            cancel = true;
+        };
     };
 
     return (
@@ -26,37 +76,68 @@ const Predmeti = () => {
                     display: 'flex',
                     flexWrap: 'wrap',
                 }}>
-                    <Button variant="outlined">Prikaži</Button>
-                    <FormControl>
-                        <InputLabel
-                            id='predmeti-select-label'
-                            onChange={prikaziPredmete}
-                        >Predmeti</InputLabel>
+                    <Button
+                        variant='contained'
+                        disableElevation
+                        sx={{
+                            width: 100,
+                            height: 50,
+                            marginRight: 1,
+                            marginBottom: 1,
+                        }}
+                        onClick={handleFetch}
+                    >
+                        Dobavi
+                    </Button>
+                    <FormControl
+                        sx={{
+                            flexDirection: 'row',
+                            flexWrap: 'wrap',
+                        }}
+                    >
                         <Select
-                            labelId='predmeti-select-label'
-                            id='predmeti-select'
-                            label='Predmeti'
+                            variant='outlined'
+                            id='predmeti-fetch-select'
+                            value={selectWhatToFetch[0]}
+                            sx={{
+                                width: 200,
+                                height: 50,
+                                marginRight: 1,
+                                marginBottom: 1,
+                            }}
+                            onChange={handleFetchSelect}
                         >
-                            <MenuItem value='0'>Sve predmete</MenuItem>
-                            <MenuItem value='1'>ID</MenuItem>
-                            <MenuItem value='2'>Razred</MenuItem>
-                            <MenuItem value='3'>Fond časova</MenuItem>
+                            <MenuItem value='0'>sve predmete</MenuItem>
+                            <MenuItem value='1'>po ID-u</MenuItem>
+                            <MenuItem value='2'>po početku naziva</MenuItem>
                         </Select>
+                        <TextField
+                            variant='outlined'
+                            id='predmeti-fetch-input'
+                            label={selectWhatToFetch[1]}
+                            sx={{
+                                display: () => selectWhatToFetch[2],
+                                flexDirection: 'row',
+                                width: 200,
+                                height: 50,
+                                marginBottom: 1,
+                            }}
+                            onChange={handleFetchParam}
+                        />
                     </FormControl>
-                    <input type='text' placeholder='Traži...' />
                 </Box>
                 <Button variant="outlined" onClick={() => navigate('novi-predmet')}>
                     Novi predmet
                 </Button>
             </Box>
             <Outlet></Outlet>
-            {/* <ol>
-                {predmeti.map((p) => {
+            <ol>
+                {fetchedData.map((p) => {
                     return (
                         <li key={p.predmet_id}>{p.naziv} {p.razred} {p.fondCasova}</li>
                     );
                 })}
-            </ol> */}
+            </ol>
         </Container>
     );
 };
