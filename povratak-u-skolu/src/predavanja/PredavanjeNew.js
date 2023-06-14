9353bc63b5db5e315bfd0c7240ac286ce08e7f7f
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-06-14 22:14:47 +0200

    Zavrseno dodavanje novog predavanja.

--- povratak-u-skolu/src/predavanja/PredavanjeNew.js
+++ povratak-u-skolu/src/predavanja/PredavanjeNew.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useRef, useState } from "react";
-import { Autocomplete, Box, Button, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
+import { Autocomplete, Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 import { UserContext } from "../App";
 import LoadingComponent from '../components/LoadingComponent';
@@ -12,9 +12,7 @@ const PredavanjeNew = () => {
     const [warning, setWarning] = useState(null);
     const [error, setError] = useState(null);
 
-    const [fetchedData, setFetchedData] = useState([]);
-
-    // Priprema podataka
+    // Dobavljanje nastavnika i predmeta sa bekenda
     const [fetchedNastavnici, setFetchedNastavnici] = useState([]);
     const [fetchedPredmeti, setFetchedPredmeti] = useState([]);
     const handleFetchPreparation = () => {
@@ -90,9 +88,8 @@ const PredavanjeNew = () => {
     // Dodavanje novog predavanja
     const nastavnik = useRef(0);
     const predmet = useRef(0);
-
-    // Generisanje dummy predmeta
-    const handleFetchDummyData = () => {
+    const [fetchedData, setFetchedData] = useState([]);
+    const handleFetchNovoPredavanje = () => {
         // reset upozorenja
         setLoading(true);
         setWarning(null);
@@ -101,97 +98,20 @@ const PredavanjeNew = () => {
         let cancel = false;
         const goFetch = async () => {
             try {
-                let response;
-                response = await fetch(`http://localhost:8080/api/v1/predmeti/dummy`, {
-                    method: "POST",
-                    headers: {
-                        'Authorization': `Bearer ${user.token}`
-                    }
-                });
-                if (response.ok) {
-                    let f = await response.json();
-                    // U slucaju da bekend nije poslao niz, prepakuje se u niz
-                    if (!Array.isArray(f)) {
-                        let notArrayFetched = f;
-                        f = [];
-                        f.push(notArrayFetched);
-                    }
-                    if (!cancel) {
-                        setFetchedData(f);
-                    }
+                // Validacija izbora
+                if (isNaN(parseInt(nastavnik.current)) || nastavnik.current < 1) {
+                    return setWarning('Molim Vas da izaberete nastavnika!');
                 }
-                else {
-                    // hvatanje neuspesnog HTTP odgovora
-                    if (response.status === 403) {
-                        setWarning(`Niste ovlašćeni da pristupite traženim resursima. (HTTP kod: ${response.status})`);
-                    }
-                    else {
-                        setWarning(`Zahtev ka serveru nije bio uspešan (HTTP kod: ${response.status})`);
-                    }
-                }
-            } catch (error) {
-                // hvatanje greski van HTTP odgovora
-                setError(new Error(error));
-            }
-            finally {
-                // reset upozorenja
-                setLoading(false);
-            }
-        };
-        goFetch();
-        return () => {
-            cancel = true;
-        };
-    };
-
-    // Dodavanje novog predmeta
-    const noviPredmetNaziv = useRef(null);
-    const noviPredmetRazred = useRef(null);
-    const noviPredmetFond = useRef(null);
-    const handleFetchNoviPredmet = () => {
-        // reset upozorenja
-        setLoading(true);
-        setWarning(null);
-        setError(null);
-
-        let cancel = false;
-        const goFetch = async () => {
-            try {
-                // Validacija i priprema vrednosti naziva
-                if (noviPredmetNaziv.current == null || noviPredmetNaziv.current === '') {
-                    return setWarning('Molim Vas da upišete naziv predmeta!');
-                }
-                const regex = new RegExp(/^\s*([a-zA-ZšđčćžŠĐČĆŽ]{3})(.*)$/);
-                if (!regex.test(noviPredmetNaziv.current)) {
-                    return setWarning('Naziv predmeta mora da počinje sa najmanje tri slovna karaktera!');
-                }
-                const strNoviPredmetNaziv = noviPredmetNaziv.current.trim().charAt(0).toUpperCase() + noviPredmetNaziv.current.trim().slice(1).toLowerCase();
-                // Validacija vrednosti razreda
-                if (isNaN(parseInt(noviPredmetRazred.current))) {
-                    return setWarning('Molim Vas da upišete razred!');
-                }
-                if (parseInt(noviPredmetRazred.current) < 1 || parseInt(noviPredmetRazred.current) > 8) {
-                    return setWarning(`Razred mora da bude 1 - 8!`);
-                }
-                // Validacija vrednosti fonda casova
-                if (isNaN(parseInt(noviPredmetFond.current))) {
-                    return setWarning('Molim Vas da upišete nedeljni fond časova!');
-                }
-                if (parseInt(noviPredmetFond.current) < 1 || parseInt(noviPredmetFond.current) > 5) {
-                    return setWarning(`Nedeljni fond časova mora da bude 1 - 5!`);
+                if (isNaN(parseInt(predmet.current)) || predmet.current < 1) {
+                    return setWarning('Molim Vas da izaberete predmet!');
                 }
                 let response;
-                response = await fetch(`http://localhost:8080/api/v1/predmeti`, {
+                response = await fetch(`http://localhost:8080/api/v1/predaju?idNastavnik=${parseInt(nastavnik.current)}&idPredmet=${parseInt(predmet.current)}`, {
                     method: "POST",
                     headers: {
                         'Authorization': `Bearer ${user.token}`,
                         'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        naziv: strNoviPredmetNaziv,
-                        razred: noviPredmetRazred.current,
-                        fondCasova: noviPredmetFond.current,
-                    }),
+                    }
                 });
                 if (response.ok) {
                     let f = await response.json();
@@ -209,6 +129,9 @@ const PredavanjeNew = () => {
                     // hvatanje neuspesnog HTTP odgovora
                     if (response.status === 403) {
                         setWarning(`Niste ovlašćeni da pristupite traženim resursima. (HTTP kod: ${response.status})`);
+                    }
+                    if (response.status === 417) {
+                        setWarning(`Nastavnik je već registrovan za predmet. (HTTP kod: ${response.status})`);
                     }
                     else {
                         setWarning(`Zahtev ka serveru nije bio uspešan (HTTP kod: ${response.status})`);
@@ -250,9 +173,9 @@ const PredavanjeNew = () => {
                 spacing={1}
             >
                 <Autocomplete
-                    getOptionLabel={(fetchedNastavnici) => `${fetchedNastavnici.korisnickoIme}`}
+                    getOptionLabel={(fetchedNastavnici) => `${fetchedNastavnici.korisnik.ime} ${fetchedNastavnici.korisnik.prezime}`}
                     options={fetchedNastavnici}
-                    isOptionEqualToValue={(option, value) => option.korisnickoIme === value.korisnickoIme}
+                    isOptionEqualToValue={(option, value) => option.nastavnik_id === value.nastavnik_id}
                     noOptionsText='Nema traženog nastavnika!'
                     renderOption={(props, fetchedNastavnici) => (
                         <Box
@@ -260,44 +183,45 @@ const PredavanjeNew = () => {
                             {...props}
                             key={fetchedNastavnici.nastavnik_id}
                         >
-                            {fetchedNastavnici.korisnickoIme}
+                            {fetchedNastavnici.korisnik.ime + ' ' + fetchedNastavnici.korisnik.prezime}
                         </Box>
                     )}
                     renderInput={(params) => <TextField {...params} label='Potraži nastavnika...' />}
                     sx={{ flexGrow: 1 }}
+                    onChange={(event, newValue) => {
+                        nastavnik.current = newValue?.nastavnik_id;
+                    }}
                 />
                 <Autocomplete
-                    options={['1', '2', '3', '4', '5']}
-                    renderInput={(params) => <TextField {...params} label='labela' />}
+                    getOptionLabel={(fetchedPredmeti) => `${fetchedPredmeti.naziv} [razred: ${fetchedPredmeti.razred}]`}
+                    options={fetchedPredmeti}
+                    isOptionEqualToValue={(option, value) => option.predmet_id === value.predmet_id}
+                    noOptionsText='Nema traženog predmeta!'
+                    renderOption={(props, fetchedPredmeti) => (
+                        <Box
+                            component='li'
+                            {...props}
+                            key={fetchedPredmeti.predmet_id}
+                        >
+                            {fetchedPredmeti.naziv + ' [razred: ' + fetchedPredmeti.razred + ']'}
+                        </Box>
+                    )}
+                    renderInput={(params) => <TextField {...params} label='Potraži predmet...' />}
                     sx={{ flexGrow: 1 }}
+                    onChange={(event, newValue) => {
+                        predmet.current = newValue?.predmet_id;
+                    }}
                 />
-                {/* <Select
-                    variant='standard'
-                    size='large'
-                    id='predavanja-nastavnici'
-                    value={nastavnik.current}
-                    sx={{ width: 190 }}
-                // onChange={handleFetchSelect}
-                >
-                    <MenuItem value='0'>izaberite nastavnika</MenuItem>
-                    {fetchedNastavnici.map((fd) => <MenuItem key={fd.nastavnik_id} value={fd.nastavnik_id}>{fd.korisnik.ime} {fd.korisnik.prezime}</MenuItem>)}
-                </Select> */}
             </Stack>
             <Box
                 textAlign={'center'}
                 mt={2}            >
                 <Button
-                    variant='outlined'
-                    size='small'
-                    sx={{ mr: 2, mb: 1 }}
-                    onClick={handleFetchDummyData}
-                >Generiši podrazumevane predmete</Button>
-                <Button
                     variant='contained'
                     size='large'
                     disableElevation
                     sx={{ mr: 2, mb: 1 }}
-                    onClick={handleFetchNoviPredmet}
+                    onClick={handleFetchNovoPredavanje}
                 >Upiši</Button>
             </Box>
             <Box
@@ -310,11 +234,18 @@ const PredavanjeNew = () => {
                 {warning && <Box sx={{ mt: 5, mb: 5 }}><WarningComponent warning={warning} /></Box>}
                 {error && <Box sx={{ mt: 5, mb: 5 }}><ErrorComponent error={error.message} sx={{ mt: 10, mb: 10 }} /></Box>}
             </Box>
-            <Stack direction='column' sx={{ padding: '0.2rem' }}>
+            <Stack
+                direction='column'
+                sx={{ padding: '0.4rem' }}
+            >
                 {fetchedData.map((fd) => {
                     return (
-                        <Typography variant='body2' key={fd.predmet_id}>
-                            [ID: {fd.predmet_id}] - {fd.naziv} za razred [{fd.razred}] sa fondom časova [{fd.fondCasova}].
+                        <Typography
+                            variant='body2'
+                            textAlign='center'
+                            key={fd.predaje_id}
+                        >
+                            Nastavnik: {fd.nastavnik.korisnik.ime} {fd.nastavnik.korisnik.prezime}, Predmet: {fd.predmet.naziv} za razred {fd.predmet.razred}.
                         </Typography>
                     );
                 })}
